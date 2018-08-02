8de286248c74a2779e8b2a2d6cb788a0d95a19fd
khempant <khempant311@gmail.com>
2018-08-02 15:44:39 +0200

    Handlercodep

--- JAKHSOApp/Handlers/TaskHandler.cs
+++ JAKHSOApp/Handlers/TaskHandler.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JAKHSOApp.Persistency;
 
 namespace JAKHSOApp.Handlers
 {
-    class TaskHandler
-    {
-    }
+     public class TaskHandler
+     {
+         private IpersistancyFactory<Model.Task> _loadTaskFromPersistencyFactory = new PersistencyFactory<Model.Task>();
+
+         public ObservableCollection<Model.Task> RetrivedUndoneTaskCollection(string targetTable)
+         {
+             ObservableCollection<Model.Task> taskCollection =
+                 _loadTaskFromPersistencyFactory.Retrivedcollection(targetTable);
+         }
+     }
 }
