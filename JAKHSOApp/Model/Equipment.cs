10de0691cd984e64d8d89940a6012e71b6703a4f
khempant <khempant311@gmail.com>
2018-08-07 20:19:36 +0200

    no changes

--- JAKHSOApp/Model/Equipment.cs
+++ JAKHSOApp/Model/Equipment.cs
@@ -15,18 +15,15 @@ namespace JAKHSOApp.Model
 
         public int EquipmentId
         {
-            get { return _equipmentId; }
-            set { _equipmentId = value; }
+            get { return _equipmentId; } set { _equipmentId = value; }
         }
         public string EquipmentName
         {
-            get { return _equipmentName; }
-            set { _equipmentName = value; }
+            get { return _equipmentName; } set { _equipmentName = value; }
         }
         public string EquipmentType
         {
-            get { return _equipmentType; }
-            set { _equipmentType = value; }
+            get { return _equipmentType; }  set { _equipmentType = value; }
         }
 
         public Equipment ( int equipmentiId,string equipmentname,string equipmenttype)
